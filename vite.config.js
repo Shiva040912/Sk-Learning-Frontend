e526bf76ca8171459b0bc411eda5d723ab6bf190
Shiva040912 <shivaarun0071@gmail.com>
2026-08-25 14:04:51 +0530

    PWA applied

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "The SK Learnings",
+        short_name: "SK Learnings",
+
+        description:
+          "The SK Learnings Fee Management System",
+
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
